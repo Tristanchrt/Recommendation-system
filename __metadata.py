58bf6590d2979ccd079b4cdf38e90a3770783ace
Victor DURAND <vic20016@gmail.com>
2022-03-22 17:14:09 +0100

    feat: stats

--- __metadata.py
+++ __metadata.py
@@ -6,36 +6,36 @@ import json
 import numpy as np
 import math
 from sklearn.cluster import KMeans
+import matplotlib.image as img
 
 
-def kmens_img(image, nb_color):
-    numarray = np.array(image.getdata(), np.uint8)
-    clusters = KMeans(n_clusters = nb_color)
-    clusters.fit(numarray)
-    npbins = np.arange(0, nb_color+1)
-    histogram = np.histogram(clusters.labels_, bins=npbins)
-
-    color_list = []
-
-    for i in range(nb_color):
-        colors = '#%02x%02x%02x' % ( math.ceil(clusters.cluster_centers_[i][0]), 
+def main_colors(imgfile):
+    numarray = np.array(imgfile.getdata(), np.uint8)
+    if len(numarray.shape) == 2:
+        clusters = KMeans(n_clusters = 5)
+        clusters.fit(numarray)
+        colors = []
+        for i in range(0,2):
+            color = '#%02x%02x%02x' % (
+                math.ceil(clusters.cluster_centers_[i][0]),
                     math.ceil(clusters.cluster_centers_[i][1]),
-                    math.ceil(clusters.cluster_centers_[i][2]))
-        color_list.append(colors)
+                math.ceil(clusters.cluster_centers_[i][2]))
+            colors.append(color)
+        return colors
+    else:
+        return ''
 
-    return sorted(histogram[0], reverse=True), color_list
 
 df=pd.read_csv('images/pokemon.csv', sep=',',header=None, skiprows=1)
 df.replace(np.nan, "")
-# print(df.values)
+id=0
 json_data = []
-id = 0
-for filename in os.listdir("images/images/"):
+for filename in os.listdir("images/images/")[:5]:
     f = "images/images/" + filename
     image = Image.open(f)
+    image = image.resize((120,120))
     metadata = df.loc[df[0] == filename.split(".")[0]]
-    # print(image.getdata())
-    # histo, colors = kmens_img(image, 2)
+
     name = metadata[0].values[0]
     id+=1
     json_metadata = {
@@ -46,11 +46,9 @@ for filename in os.listdir("images/images/"):
             "type2" : metadata[2].replace(np.nan, "None").values[0]
         },
         "size" : image.size,
-        "colors" : '',
-        "tags" : [],
-        "path" : f 
+        "main_colors" : main_colors(image),
+        "path" : f
     }
-    # json_metadata = json.dumps(json_metadata)
     json_data.append(json_metadata)
 with open("images/metadata/metadata.json", 'w+') as outfile:
     outfile.write(json.dumps(json_data))
--- __stats.py
+++ __stats.py
@@ -1,19 +1,28 @@
 import json
 import pandas as pd
+import matplotlib.pyplot as plt
 with open('images/metadata/metadata.json','r') as f:
     data = json.loads(f.read())
 # df = pd.read_json('images/metadata/metadata.json')
 
 df = pd.json_normalize(
-    data, 
-    record_path =['students'], 
+    data,  
     meta=[
         'class',
         ['properties', 'type1', 'types2'], 
-        # ['info', 'contacts', 'tel']
     ]
 )
 
-df.values()
+grouped_df = df.groupby(['properties.type1'])['properties.type1']
+print(grouped_df.describe())
+x = []
+y = []
+for key, item in grouped_df:
+    group = grouped_df.get_group(key)
+    x.append(key)
+    y.append(group.count())
 
+plt.bar(x,y )
+plt.show()
+# print(stats)
 
